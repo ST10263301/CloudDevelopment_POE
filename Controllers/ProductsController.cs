a30af2a28061d7ba413af8b3155a7d7fd249fdbf
ST10263301 <ST10263301@vcconnect.edu.za>
2024-04-11 10:57:09 +0200

    Add project files.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CloudDevelopment.Controllers
+{
+   
+        public class ProductController : Controller
+        {
+            public productTable prodtbl = new productTable();
+
+
+
+            [HttpPost]
+            public ActionResult MyWork(productTable products)
+            {
+                var result2 = prodtbl.insert_product(products);
+                return RedirectToAction("Index", "Home");
+            }
+
+            [HttpGet]
+            public ActionResult MyWork()
+            {
+                return View(prodtbl);
+            }
+        }
+    
+}
