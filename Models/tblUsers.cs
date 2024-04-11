30337b6ccf4f78d4b229aff594605166f23be7a1
ST10263301 <ST10263301@vcconnect.edu.za>
2024-04-11 11:00:51 +0200

    init

--- Models/tblUsers.cs
+++ Models/tblUsers.cs
@@ -1,12 +1,12 @@
-﻿namespace CloudDevelopment.Models
+﻿using System.Data.SqlClient;
+
+namespace CloudDevelopment.Models
 {
     public class tblUsers
     {
         public static string con_string = "Server=tcp:keenia-server.database.windows.net,1433;Initial Catalog=keenia-db;Persist Security Info=False;User ID=keenia;Password= Geemooi2005!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30; ";
 
         public static SqlConnection con = new SqlConnection(con_string);
-
-
 
         public string Name { get; set; }
 
