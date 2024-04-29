d96f9ded130418f6feeb91073c22d2fe64094dcc
ST10263301 <ST10263301@vcconnect.edu.za>
2024-04-29 12:40:27 +0200

    con change

--- Models/tblUsersModel.cs
+++ Models/tblUsersModel.cs
@@ -6,6 +6,7 @@ namespace CloudDevelopment.Models
     {
         public static string con_string = "Server=tcp:keenia-server.database.windows.net,1433;Initial Catalog=st10263301-db;Persist Security Info=False;User ID=keenia;Password=Geemooi2005!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
+
         public static SqlConnection con = new SqlConnection(con_string);
 
         public string Name { get; set; }
